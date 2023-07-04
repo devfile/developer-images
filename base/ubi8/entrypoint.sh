4386d99800788de406189d4115e23f86f76bf45d
musienko maksym <mmusiien@redhat.com>
2023-07-04 15:22:16 +0300

    fake pr

--- base/ubi8/entrypoint.sh
+++ base/ubi8/entrypoint.sh
@@ -15,6 +15,7 @@ if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
     echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
     echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
+    echo ">>>>>>>>>>>>"
   fi
 fi
 
