1dc976779701bef98cc06698254c5a8e9ab1f859
Andrew Obuchowicz <aobuchow@redhat.com>
2023-09-20 17:18:00 -0400

    Remove obsolete ~/.bashrc creation code from base image entrypoint
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- base/ubi8/entrypoint.sh
+++ base/ubi8/entrypoint.sh
@@ -5,11 +5,6 @@ if [ ! -d "${HOME}" ]; then
   mkdir -p "${HOME}"
 fi
 
-# Setup $PS1 for a consistent and reasonable prompt
-if [ -w "${HOME}" ] && [ ! -f "${HOME}"/.bashrc ]; then
-  echo "PS1='[\u@\h \W]\$ '" > "${HOME}"/.bashrc
-fi
-
 # Add current (arbitrary) user to /etc/passwd and /etc/group
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
