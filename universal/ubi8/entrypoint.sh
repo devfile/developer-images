18b26a72f7ef75ce20889a34d543b3e944729361
Andrew Obuchowicz <aobuchow@redhat.com>
2023-10-02 15:12:18 -0400

    Ensure ~/.bashrc and ~/.bash_profile exist when persistUserHome enabled
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- universal/ubi8/entrypoint.sh
+++ universal/ubi8/entrypoint.sh
@@ -62,6 +62,10 @@ if [ $HOME_USER_MOUNTED -eq 0 ] && [ ! -f $STOW_COMPLETE ]; then
     stow . -t /home/user/ -d /home/tooling/ --no-folding -v 2 > /tmp/stow.log 2>&1
     # Vim does not permit .viminfo to be a symbolic link for security reasons, so manually copy it
     cp /home/tooling/.viminfo /home/user/.viminfo
+    # We have to restore bash-related files back onto /home/user/ (since they will have been overwritten by the PVC)
+    # but we don't want them to be symbolic links (so that they persist on the PVC)
+    cp /home/tooling/.bashrc /home/user/.bashrc
+    cp /home/tooling/.bash_profile /home/user/.bash_profile
     touch $STOW_COMPLETE
 fi
 
