888d46a2f6087ab8b4c188e258793a15e2bc809b
David Kwon <dakwon@redhat.com>
2024-08-08 09:25:35 -0400

    Update ubi to configure kubedock and podman (#179)
    Add podman, buildah, skopeo, fuse and kubedock functionality from developer image to base image
    
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- base/ubi8/entrypoint.sh
+++ base/ubi8/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Ensure $HOME exists when starting
-if [ ! -d "${HOME}" ]; then
-  mkdir -p "${HOME}"
-fi
-
-# Add current (arbitrary) user to /etc/passwd and /etc/group
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
-    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
-  fi
-fi
+source kubedock_setup
 
 exec "$@"
