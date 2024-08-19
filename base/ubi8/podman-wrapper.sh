ecac1599be8ec57b04cc490a51f6bbd1ccdbb6de
David Kwon <dakwon@redhat.com>
2024-08-19 17:40:52 -0400

    Remove /home/tooling/.local (#181)
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- base/ubi8/podman-wrapper.sh
+++ base/ubi8/podman-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-ORIGINAL_PODMAN_PATH=${ORIGINAL_PODMAN_PATH:-"/usr/bin/podman.orig"}
+PODMAN_ORIGINAL_PATH=${PODMAN_ORIGINAL_PATH:-"/usr/bin/podman.orig"}
 KUBEDOCK_SUPPORTED_COMMANDS=${KUBEDOCK_SUPPORTED_COMMANDS:-"run ps exec cp logs inspect kill rm wait stop start"}
 
 PODMAN_ARGS=( "$@" )
@@ -10,11 +10,11 @@ TRUE=0
 FALSE=1
 
 exec_original_podman() {
-    exec ${ORIGINAL_PODMAN_PATH} "${PODMAN_ARGS[@]}"
+    exec ${PODMAN_ORIGINAL_PATH} "${PODMAN_ARGS[@]}"
 }
 
 exec_kubedock_podman() {
-    exec env CONTAINER_HOST=tcp://127.0.0.1:2475 "${ORIGINAL_PODMAN_PATH}" "${PODMAN_ARGS[@]}"
+    exec env CONTAINER_HOST=tcp://127.0.0.1:2475 "${PODMAN_ORIGINAL_PATH}" "${PODMAN_ARGS[@]}"
 }
 
 podman_command() {
