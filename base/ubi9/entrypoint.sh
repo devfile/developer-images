07e19f5e9099f18d006f66b68548669f7fdce57c
David Kwon <dakwon@redhat.com>
2025-08-06 13:51:07 -0400

    Add .copy-files file which copies over files from /home/tooling to /home/user in the entrypoint
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- base/ubi9/entrypoint.sh
+++ base/ubi9/entrypoint.sh
@@ -90,12 +90,17 @@ if [ $HOME_USER_MOUNTED -eq 0 ] && [ ! -f $STOW_COMPLETE ]; then
     #
     # Create symbolic links from /home/tooling/ -> /home/user/
     stow . -t /home/user/ -d /home/tooling/ --no-folding -v 2 > /tmp/stow.log 2>&1
-    # Vim does not permit .viminfo to be a symbolic link for security reasons, so manually copy it
-    cp /home/tooling/.viminfo /home/user/.viminfo
-    # We have to restore bash-related files back onto /home/user/ (since they will have been overwritten by the PVC)
-    # but we don't want them to be symbolic links (so that they persist on the PVC)
-    cp /home/tooling/.bashrc /home/user/.bashrc
-    cp /home/tooling/.bash_profile /home/user/.bash_profile
+
+    # Read .copy-files and copy files from /home/tooling to /home/user
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        # Skip empty and commented lines
+        [[ -z "$line" || "$line" == \#* ]] && continue
+
+        tooling_path=$(realpath "/home/tooling/$line")
+        user_path=$(realpath "/home/user/$line")
+        cp --no-clobber -r $tooling_path $user_path
+    done < /home/tooling/.copy-files
+
     touch $STOW_COMPLETE
 fi
 
